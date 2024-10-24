487440341edeb4b55af1a23d4c992974220b8cd7
mursidx <iammursid@gmail.com>
2024-10-24 18:37:46 +0530

    Initial commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://mursid_notes:mursid786@cluster0.afrcf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect(process.env.MONGODB_URI)
 const db = mongoose.connection;
 
 //set up debugging
 require('dotenv').config();
 const debuglog = require('debug')('development:mongoose');
-
 
 db.on('error',function(err){
     debuglog(err)
